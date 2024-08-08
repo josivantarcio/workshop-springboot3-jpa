8fbc1d0519765afaa2bf5e6d653190e7dd06f26d
Josevan Oliveira <josivantarcio@msn.com>
2024-08-08 15:42:48 -0300

    user insert

--- src/main/java/com/datalifebrazil/course/resources/UserResource.java
+++ src/main/java/com/datalifebrazil/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.datalifebrazil.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.datalifebrazil.course.entities.User;
 import com.datalifebrazil.course.services.UserService;
@@ -24,11 +28,19 @@ public class UserResource {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
-	@GetMapping(value= "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 
 }
--- src/main/java/com/datalifebrazil/course/services/UserService.java
+++ src/main/java/com/datalifebrazil/course/services/UserService.java
@@ -23,5 +23,9 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
 
 }
