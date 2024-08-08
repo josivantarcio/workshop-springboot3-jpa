7761fae46aff416223cf873107c9b93b86a9757d
Josevan Oliveira <josivantarcio@msn.com>
2024-08-08 14:59:43 -0300

    payment, one-to-ne associatiion

--- src/main/java/com/datalifebrazil/course/config/TestConfig.java
+++ src/main/java/com/datalifebrazil/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.datalifebrazil.course.entities.Category;
 import com.datalifebrazil.course.entities.Order;
 import com.datalifebrazil.course.entities.OrderItem;
+import com.datalifebrazil.course.entities.Payment;
 import com.datalifebrazil.course.entities.Product;
 import com.datalifebrazil.course.entities.User;
 import com.datalifebrazil.course.entities.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig<S> implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 
 	}
 }
--- src/main/java/com/datalifebrazil/course/entities/Order.java
+++ src/main/java/com/datalifebrazil/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.datalifebrazil.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,6 +41,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = (CascadeType.ALL))
+	private Payment payment;
 
 	public Order() {
 	}
@@ -74,6 +79,14 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public User getClient() {
