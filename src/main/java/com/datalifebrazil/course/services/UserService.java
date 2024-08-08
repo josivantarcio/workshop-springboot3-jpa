95a54e7cc8c24cf80e86688525beaaa73a6d4139
Josevan Oliveira <josivantarcio@msn.com>
2024-08-08 17:13:26 -0300

    user update

--- src/main/java/com/datalifebrazil/course/services/UserService.java
+++ src/main/java/com/datalifebrazil/course/services/UserService.java
@@ -23,13 +23,26 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User user) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, user);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+
 	}
 
 }
